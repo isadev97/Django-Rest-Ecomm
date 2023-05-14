f90b3ea1dee52a36c62aa425b167406e8a3ce359
isadev97 <isa325697@gmail.com>
2023-05-14 22:30:11 +0530

    updated tags model

--- tags/models.py
+++ tags/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Tag(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, unique=True)
     slug = models.CharField(max_length=30, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- tags/views.py
+++ tags/views.py
@@ -3,27 +3,6 @@ from rest_framework.response import Response
 
 class CreateTag(APIView):
     
-    authentication_classes 
-    permission_classes
-    throttle_classes
-    serializer 
-    
-    
-    pagination (List)
-    filters (List)
-    
-    for a same url you can support upto 4 http methods 
-    
-    def get(self, request):
-        pass
-    
     def post(self, request):
-        pass 
         print("post data coming")
     
-    def put(self, request):
-        pass 
-    
-    
-    def delete(self, request):
-        pass 
